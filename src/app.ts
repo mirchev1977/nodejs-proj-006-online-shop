19dca0730209e181a30ffad575fc70c274fd8d0f
Miroslav Mirchev <mirchev1977@yahoo.com>
2019-09-23 19:38:49 +0300

    Implemented the Hello world in express

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+
+app.get( '/', ( req, res, next ) => {
+    res.write( 'Hello, world!' );
+    res.end();
+} );
+
+app.listen( process.env.PORT || 3000, () => {
+    console.log( 'Listening on port 3000' );
+} );
