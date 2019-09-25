83cd15a4c9fed895c1174bbdf3fd7d45cfba32e5
Miroslav Mirchev <mirchev1977@yahoo.com>
2019-09-25 06:00:58 +0300

    Handle Error on User Creation

--- dist/controllers/controllers-user.js
+++ dist/controllers/controllers-user.js
@@ -5,13 +5,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const entities_user_1 = __importDefault(require("../entities/entities-user"));
 function getUsrRegister(req, res, next) {
-    res.render('user/register');
+    res.render('user/register', { usr: {} });
 }
 exports.getUsrRegister = getUsrRegister;
 function postUsrRegister(req, res, next) {
-    const usr = new entities_user_1.default(req.body['names'], req.body['email'], req.body['password'], req.body['password_repeat']);
-    console.log('success: ', usr);
-    res.render('user/registered');
+    try {
+        const user = new entities_user_1.default(req.body['names'], req.body['email'], req.body['password'], req.body['password_repeat']);
+        res.render('user/registered');
+    }
+    catch (errMsg) {
+        res.render('user/register', { usr: req.body, ERR: errMsg });
+    }
 }
 exports.postUsrRegister = postUsrRegister;
 //# sourceMappingURL=controllers-user.js.map
--- src/controllers/controllers-user.ts
+++ src/controllers/controllers-user.ts
@@ -1,15 +1,20 @@
 import User from '../entities/entities-user';
 
 export function getUsrRegister ( req, res, next ) {
-    res.render( 'user/register' );
+    res.render( 'user/register', { usr: {} } );
 }
 
 export function postUsrRegister ( req, res, next ) {
-    const usr = new User( 
-            req.body[ 'names'           ],
-            req.body[ 'email'           ],
-            req.body[ 'password'        ],
-            req.body[ 'password_repeat' ]
-        );
-    res.render( 'user/registered' );
+    try {
+        const user = new User( 
+                req.body[ 'names'           ],
+                req.body[ 'email'           ],
+                req.body[ 'password'        ],
+                req.body[ 'password_repeat' ]
+            );
+
+        res.render( 'user/registered' );
+    } catch( errMsg ) {
+        res.render( 'user/register', { usr: req.body, ERR: errMsg } );
+    }
 }
