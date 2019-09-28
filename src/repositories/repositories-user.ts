5869952e0cebb9a8dd695a943b6b192af8147de0
Miroslav Mirchev <mirchev1977@yahoo.com>
2019-09-28 05:48:10 +0300

    Some Code Formatting

--- src/repositories/repositories-user.ts
+++ src/repositories/repositories-user.ts
@@ -2,8 +2,9 @@ import Sequelize from 'sequelize';
 const  sequelize = require( '../utils/database' );
 
 const User = sequelize.define( 'user', {
-    id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true, allowNull: false },
-    names: Sequelize.STRING,
+    id:       { type: Sequelize.INTEGER, autoIncrement: true, 
+                primaryKey: true, allowNull: false },
+    names:    Sequelize.STRING,
     email:    { type: Sequelize.STRING, allowNull: false },
     password: { type: Sequelize.STRING, allowNull: false }
 } );
